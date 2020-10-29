001b4e2dc331864ed4dc97037dde24799654a928
Karol Pichurski <71383791+Khaotich@users.noreply.github.com>
2020-10-29 10:46:17 +0100

    Update zadanie10.py

--- LISTA2/zadanie10.py
+++ LISTA2/zadanie10.py
@@ -1,10 +1,6 @@
 lista = [x for x in range(3,100,3)]
 
-for i in range(5,len(lista),3):
-    try:
-        del lista[i]
-    except:
-        break
+del lista [5::3]
 
 srednia = sum(lista)/len(lista)
 print(srednia)
